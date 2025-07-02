aee794247af2e8f6213b9e004571dd8310ce45b9
Augustine-2049 <913313180@qq.com>
2025-07-02 16:12:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 金融研报自动生成系统
+基于 agno 工作流引擎的智能金融研报生成系统，支持行业研究和宏观经济分析。
+技术栈：agno\MCP\agent\prompt
+
+# 备注
+1. 使用千问模型qwen-turbo，注意最新的千问模型需要传enable_thinking，还不知咋搞。
---
+++ main.py
@@ -0,0 +1,31 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAILike
+import os
+from agno.tools import tool
+
+qw_model = OpenAILike(
+    id= 'qwen-turbo', # qwen-turbo / qwen3-235b-a22b
+    api_key='sk-295ced4ab5a042d3949d48232553c078',
+    base_url='https://dashscope.aliyuncs.com/compatible-mode/v1/',
+    
+    # extra_body={"chat_template_kwargs": {"enable_thinking": False}},
+    # parameters={"enable_thinking": False},  # 添加这行关键参数
+    # enable_thinking=False,
+)
+
+@tool()
+def add_tools(x: int, y: int) -> str:
+    return x + y
+
+agent = Agent(
+    model=qw_model,
+    tools=[add_tools],
+    instructions="请你使用中文回答问题",
+    markdown=True,
+    debug_mode=True,
+    # extra_body={"chat_template_kwargs": {"enable_thinking": False}},
+)
+
+agent.run("请计算1 + 2 /no_think")
+
+
