3622421e198f95d8a72e7a93d006d31ccf80f72d
huangzijian <913313180@qq.com>
2025-07-04 01:33:17 +0800

    fix level_1

--- level_1_agent.py
+++ level_1_agent.py
@@ -3,13 +3,10 @@ from agno.models.anthropic import Claude
 from agno.models.openai import OpenAILike
 from agno.tools.yfinance import YFinanceTools
 
-qw_model = OpenAILike(
-    id= 'qwen-turbo', # qwen-turbo / qwen3-235b-a22b
-    api_key='sk-295ced4ab5a042d3949d48232553c078',
-    base_url='https://dashscope.aliyuncs.com/compatible-mode/v1/',
-)
+
+from agno.models.google import Gemini
 agent = Agent(
-    model=qw_model,
+    model=Gemini(id="gemini-2.0-flash"),
     tools=[YFinanceTools(stock_price=True)],
     instructions="Use tables to display data. Don't include any other text.",
     markdown=True,
